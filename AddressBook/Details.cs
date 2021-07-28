c8590a5c91e434024d08238ecd06f67d18955b02
suhasshettyy <scshetty07@gmail.com>
2021-07-28 20:35:17 +0530

    [Suhas] Add: UC1-Details

---
+++ AddressBook/Details.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    class Details
+    {
+        string Firstname, Lastname, Address, City, State, Mail;
+        int Zip;
+        long Phonenumber;
+
+
+        public Details(string firstname, string lastname, string address, string state, string city, int zip, long phonenumber, string email)
+        {
+            this.Firstname = firstname;
+            this.Lastname = lastname;
+            this.Address = address;
+            this.City = city;
+            this.State = state;
+            this.Phonenumber = phonenumber;
+            this.Zip = zip;
+            this.Mail = email;
+        }
+            public void Contact()
+            {
+                Console.WriteLine("FirstName : " + this.Firstname + "\n" + "LastName : " + this.Lastname + "\n" + "Address : " + this.Address + "\n"
+                    + "State : " + this.State + "\n" + "City : " + this.City + "\n" + "Zip : " + this.Zip + "\n"
+                    + "PhoneNumber : " + this.Phonenumber + "\n" + "Mail Id : " + this.Mail);
+            }
+    }
+}
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -7,6 +7,8 @@ namespace AddressBook
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program");
+            Details details = new Details("Suhas","Shetty","Moodbidri","Karnataka","Mangalore",574227,7892112412,"scshetty07@gamil.com");
+            details.Contact();
         }
     }
 }
