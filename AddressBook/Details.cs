cb98bff6a0c7b6e3ed8b6cec219c01bfeb86e1bd
suhasshettyy <scshetty07@gmail.com>
2021-07-29 08:32:36 +0530

    [Suhas] Add: UC2-NewContact

--- AddressBook/Details.cs
+++ AddressBook/Details.cs
@@ -1,18 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace AddressBook
 {
-    public class Details
+    class Program
     {
-        public string Firstname { get; set; }
-        public string Lastname { get; set; }
-        public string Address { get; set; }
-        public string State { get; set; }
-        public string City { get; set; }
-        public string Zip { get; set; }
-        public string Phonenumber { get; set; }
-        public string Mail { get; set; }
+
+        public static List<Details> list = new List<Details>();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book");
+            Program program = new Program();
+            program.AddDetails();
+            foreach (var person in list)
+            {
+                program.Contactdisplay(person);
+            }
+        }
+        public void AddDetails()
+        {
+            Details addressbook = new Details();
+            Console.WriteLine("enter Firstname,LastName,Adress,State,City,Zip,Phonenumber,Mail");
+            addressbook.Firstname = Console.ReadLine();
+            addressbook.Lastname = Console.ReadLine();
+            addressbook.Address = Console.ReadLine();
+            addressbook.State = Console.ReadLine();
+            addressbook.City = Console.ReadLine();
+            addressbook.Zip = Console.ReadLine();
+            addressbook.Phonenumber = Console.ReadLine();
+            addressbook.Mail = Console.ReadLine();
+            list.Add(addressbook);
+        }
+        public void Contactdisplay(Details person)
+        {
+            Console.WriteLine("FirstName : " + person.Firstname + "\n" + "LastName : " + person.Lastname + "\n" + "Address : " + person.Address + "\n"
+                           + "State : " + person.State + "\n" + "City : " + person.City + "\n" + "Zip : " + person.Zip + "\n"
+                           + "PhoneNumber : " + person.Phonenumber + "\n" + "Mail Id : " + person.Mail);
+        }
     }
 }
